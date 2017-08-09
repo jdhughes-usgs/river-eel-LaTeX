02f2504248d0c84bf996669f98f088b320d59eda
Joseph D Hughes <jdhughes@usgs.gov>
2017-08-09 16:43:40 -0400

    post-commit

---
+++ post-commit.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo 'Running post-commit hook with python'
+python post-commit.py
+
+echo 'Finished post-commit hook'
--- pre-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo 'Running update hook with python'
-python pre-push.py
-
-echo 'Finished update hook'
